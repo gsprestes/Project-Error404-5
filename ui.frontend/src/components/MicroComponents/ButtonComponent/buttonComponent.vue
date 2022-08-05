45180b02160a23c3ef6a3a77ef3d96ebca4c9a49
gsprestes~ <guilherme26prestes@gmail.com>
2022-08-05 11:51:37 -0300

    Feat: added button component

---
+++ ui.frontend/src/components/MicroComponents/ButtonComponent/buttonComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <button :style="{'font-family': typeFont}" :id="buttonStyle">{{ buttonMsg }}</button>
+</template>
+
+<script>
+export default {
+    name: "buttonComponent",
+    props: {
+        buttonMsg: {
+            type: String,
+            default: "BACK TO HOMEPAGE"
+        },
+        buttonStyle: {
+            type: String
+        },
+        typeFont: {
+            type: String
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@700&family=Montserrat:wght@500;700&family=Space+Mono:wght@400;700&display=swap');
+@import './buttonComponent.scss';
+</style>
