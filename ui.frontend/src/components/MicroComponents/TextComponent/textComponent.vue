95ce6eb8d839ad8215b6237cd5386be0581266c1
gsprestes~ <guilherme26prestes@gmail.com>
2022-08-05 11:36:50 -0300

    Feat: added text component

---
+++ ui.frontend/src/components/MicroComponents/TextComponent/textComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <p :style="{'font-family': typeFont}" :id="textStyle">{{ textMsg }}</p>
+</template>
+
+<script>
+export default {
+  name: "textComponent",
+  props: {
+    textMsg: {
+      type: String
+    },
+    textStyle: {
+      type: String
+    },
+    typeFont: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Inconsolata:wght@700&family=Montserrat:wght@500;700&family=Space+Mono:wght@400;700&display=swap');
+@import './textComponent.scss';
+</style>
